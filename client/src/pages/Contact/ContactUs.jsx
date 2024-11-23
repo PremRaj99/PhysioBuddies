ab24a08a9ffbdcc1633715613243310d6cdaae58
Prem Raj <premraj299341@gmail.com>
2024-11-24 01:06:28 +0530

    Refactor Hero and ContactUs components for improved styling and layout consistency; adjust heading sizes and spacing for better readability

--- client/src/pages/Contact/ContactUs.jsx
+++ client/src/pages/Contact/ContactUs.jsx
@@ -4,7 +4,7 @@ import physio from "../../assets/images/physio.gif";
 export default function ContactUs() {
   return (
     <>
-      <h2 className="text-xl font-semibold text-green-950 text-center bg-green-50 py-16">
+      <h2 className="text-2xl font-semibold text-green-950 text-center bg-green-50 pb-8">
         Contact Us
       </h2>
 
@@ -12,107 +12,76 @@ export default function ContactUs() {
       <div className="bg-green-50 flex flex-wrap justify-center ">
         {/* Text Sention */}
         <div className="bg-green-100 w-auto md:w-1/2 px-8 py-4  text-gray-800">
-          <h1 className="text-xl text-gray-700">
-            BOOK ONLINE
-          </h1>
+          <h1 className="text-xl text-gray-700">BOOK ONLINE</h1>
           <h2 className="md:text-3xl text-2xl my-4 text-green-950 font-[SourGummy]">
-            Relieve Pain, Restore Movement, Revive
-            Your Life
+            Relieve Pain, Restore Movement, Revive Your Life
           </h2>
-          <span className="font-semibold">
-            Physio Buddies
-          </span>{" "}
-          has helped countless individuals regain
-          mobility and improve their quality of
-          life through personalized physiotherapy
-          treatments. Our licensed practitioners
-          are available for:{" "}
+          <span className="font-semibold">Physio Buddies</span> has helped
+          countless individuals regain mobility and improve their quality of
+          life through personalized physiotherapy treatments. Our licensed
+          practitioners are available for:{" "}
           <ul className="list-disc hidden md:block px-5 py-2">
             <li>
-              <span className="font-semibold">
-                In-Clinic Sessions:
-              </span>{" "}
-              Conveniently located in your
-              Locality.
+              <span className="font-semibold">In-Clinic Sessions:</span>{" "}
+              Conveniently located in your Locality.
             </li>
             <li>
-              <span className="font-semibold">
-                Home Visits:
-              </span>{" "}
-              We come to you for comfort and
-              convenience.
+              <span className="font-semibold">Home Visits:</span> We come to you
+              for comfort and convenience.
             </li>
             <li>
-              <span className="font-semibold">
-                Virtual Appointments:
-              </span>{" "}
-              Ideal for remote locations or those
-              who prefer private care from the
+              <span className="font-semibold">Virtual Appointments:</span> Ideal
+              for remote locations or those who prefer private care from the
               comfort of their home.
             </li>
           </ul>
           <div className="w-full ">
-            <img
-              src={physio}
-              alt="physio"
-              className="sm:w-1/2 mx-auto w-4/5"
-            />
+            <img src={physio} alt="physio" className="sm:w-1/2 mx-auto w-4/5" />
           </div>
         </div>
 
         {/* Form Section */}
-        <div className="w-auto md:w-1/2 px-8 py-">
+        <div className="w-auto md:w-1/2 px-8 py-2">
           <form className="text-gray-800">
-            <h3 className="text-2xl md:text-3xl text-green-950 my-4">
-              Ready to book an appointment with a
-              licensed physiotherapist?
+            <h3 className="text-xl md:text-2xl text-green-950 my-8">
+              Ready to book an appointment with a licensed physiotherapist?
             </h3>
-            <p className="text-xl">
-              Fill out the form below, and a
-              member of our team will reach out to
-              you shortly.
+            <p className="text-base">
+              Fill out the form below, and a member of our team will reach out
+              to you shortly.
             </p>
-            <div className="flex flex-col xl-custom:flex-row gap-4 my-4">
-              <label >
-                Full Name
-              </label>
+            <div className="flex flex-col xl-custom:flex-row xl:items-center gap-2 my-4">
+              <label>Full Name</label>
               <input
                 type="text"
                 id="name"
-                className="border-2 border-green-800 rounded-md px-2 py-1"
+                className="flex-1 border-2 border-green-800 rounded-md px-2 py-1"
+                placeholder="Full Name"
               />
-              <label >
-                Phone Number
-              </label>
+              <label>Phone Number</label>
               <input
                 type="telephone"
                 id="name"
-                className="border-2 border-green-800 rounded-md px-2 py-1 ml-1"
+                className="flex-1 border-2 border-green-800 rounded-md px-2 py-1 ml-1"
+                placeholder="+91 **********"
               />
             </div>
             <div className="flex flex-col gap-4 my-4">
-              <label >
-                Email
-              </label>
+              <label>Email</label>
               <input
                 type="email"
                 id="email"
                 className="border-2 border-green-800 rounded-lg px-2 py-1 "
+                placeholder="yourname@example.com"
               />
-              <label>
-                Please describe your injury or
-                concern
-              </label>
+              <label>Please describe your injury or concern</label>
               <textarea
                 name="describe"
                 id=""
                 className="border-2 border-green-800 rounded-md px-2 py-1"
-              >
-                Briefly describe why you need
-                physiotherapy
-              </textarea>
-
-              <div className="flex flex-wrap gap-8">
+                placeholder="Briefly describe why you need physiotherapy"
+              />
+              <div className="flex flex-wrap gap-2">
                 <input
                   type="date"
                   name="date"
