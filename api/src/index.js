3343246e97231a2fe0c5b6475a7c89caa10256bc
Prem Raj <premraj299341@gmail.com>
2024-12-17 05:14:15 +0530

    Ready for deploy

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,20 @@
 import dotenv from "dotenv";
+import connectToDb from "./config/connectToDb.js";
+import app from "./app.js";
+// import path from "path";
+
 dotenv.config();
 
-import connectDb from "./db/connectDb.js";
-import app from "./app.js";
+// const __dirname = path.resolve();
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 3000;
-
-await connectDb();
+await connectToDb();
 app.listen(port, () => {
   console.log("Server is running on http://localhost:" + port);
 });
+
+// app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
